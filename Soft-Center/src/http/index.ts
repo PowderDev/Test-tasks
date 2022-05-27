464766f1258e26fc59e16cb03275670e5927afe6
Timur <makarov-timyr@yandex.ru>
2022-05-27 18:21:40 +0300

    Soft-Center fix

--- Soft-Center/src/http/index.ts
+++ Soft-Center/src/http/index.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const $api = axios.create({
   baseURL: "https://bonus-test.evoapp.ru/api/3rdparty",
   headers: {
-    Authorization: "435b7c13-ecaf-4265-83c8-186cca3242cc",
+    Authorization: import.meta.env._APIKEY,
   },
 });
