58fec7b5427e001311e71604aecf8bf83b45e119
Timur <makarov-timyr@yandex.ru>
2022-06-08 16:14:16 +0300

    Technesis

---
+++ Technesis/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main">
+    <div class="main__tags-block">
+      <TextTags :tags="tags" :showDivider="true" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TextTags from "./components/TextTags.vue";
+
+const tags = [
+  { text: "Beautiful", icon: "mdi-heart" },
+  { text: "Notebook", icon: "mdi-note" },
+  { text: "Schedule", icon: "mdi-calendar" },
+  { text: "Beautiful", icon: "mdi-heart" },
+  { text: "Notebook", icon: "mdi-note" },
+  { text: "Schedule", icon: "mdi-calendar" },
+  { text: "Beautiful", icon: "mdi-heart" },
+  { text: "Schedule", icon: "mdi-calendar" },
+  { text: "Beautiful", icon: "mdi-heart" },
+  { text: "Notebook", icon: "mdi-note" },
+  { text: "Notebook", icon: "mdi-note" },
+  { text: "Schedule", icon: "mdi-calendar" },
+  { text: "Beautiful", icon: "mdi-heart" },
+  { text: "Notebook", icon: "mdi-note" },
+  { text: "Schedule", icon: "mdi-calendar" },
+  { text: "Beautiful", icon: "mdi-heart" },
+];
+</script>
+
+<style lang="scss">
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+
+  &__tags-block {
+    width: 570px;
+    height: 270px;
+    resize: both;
+    overflow: auto;
+  }
+}
+</style>
---
+++ Technesis/src/components/TextTags.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tags-container" :class="{ left: alignment == 'left' }">
+    <div v-for="tag in tags" :key="Math.random()" class="tag">
+      <v-chip color="primary" variant="outlined" label>
+        <v-icon v-if="tag.icon" start>{{ tag.icon }}</v-icon>
+        <figure>{{ tag.text }}</figure>
+      </v-chip>
+      <v-icon v-if="showDivider" small class="divider">mdi-arrow-right</v-icon>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Tag {
+  text: string;
+  icon: null | string;
+}
+
+const {
+  alignment = "width",
+  tags,
+  showDivider = false,
+} = defineProps<{
+  alignment?: "left" | "width";
+  tags: Tag[];
+  showDivider: boolean;
+}>();
+</script>
+
+<style scoped lang="scss">
+.tags-container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  padding: 1rem;
+  border: 1px solid rgb(51, 50, 50);
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background-color: rgb(243, 243, 243);
+
+  &.left {
+    align-items: flex-start;
+    flex-wrap: nowrap;
+    flex-direction: column;
+  }
+
+  .tag {
+    display: flex;
+    align-items: center;
+    gap: 0.1rem;
+
+    .divider {
+      font-size: 18px;
+    }
+
+    &:last-child {
+      .divider {
+        display: none;
+      }
+    }
+  }
+}
+</style>
